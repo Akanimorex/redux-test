5fc8a1f8919e293faaac3d2330f7347ff5ca9322
Akanimorex <akanimoime2016@gmail.com>
2022-11-14 14:14:39 +0100

    old method of react redux, have a bug though

---
+++ src/redux/cake/cakeReducer.js
@@ -0,0 +1,23 @@
+import {BUY_CAKE} from './cakeTypes';
+
+
+
+const initialState = {
+    numOfCakes: 10
+};
+
+
+const cakeReducer = (state=initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes:initialState.numOfCakes - 1
+        }
+        default:
+            return state
+    }
+}
+
+
+
+export default cakeReducer;
